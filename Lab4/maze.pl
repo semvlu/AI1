7c59f206d9fafaa29e8183f96fd98ba3d7e26f1f
semvlu <74308718+semvlu@users.noreply.github.com>
2023-06-07 11:53:08 +0200

    Add files via upload

--- Lab4/maze.pl
+++ Lab4/maze.pl
@@ -13,9 +13,20 @@ link(l,p).
 link(m,n).
 link(n,o).
 
+link(b,a).
+link(f,b).
+link(d,c).
+link(g,c).
+link(h,d).
+link(f,e).
+link(j,f).
+link(k,g).
+link(l,h).
+link(m,i).
+link(k,j).
+link(p,l).
+link(n,m).
+link(o,n).
 
 path(X,Y) :- link(X,Y).
 path(X,Y) :- path(X,Z), link(Z,Y).
-
-path(a,p)
-path(a,m)
